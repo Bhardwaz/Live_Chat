b39b5c00855de0db33f693163bb3a905fd41ae4f
Sumit Bhardwaj <sumitbhardwaj106@gmail.com>
2023-05-21 17:07:44 +0530

    store & slices

---
+++ utils/constants.js
@@ -0,0 +1,1 @@
+export const IMG_URL = "https://media.licdn.com/dms/image/D4D03AQEvA4Tm335KxQ/profile-displayphoto-shrink_800_800/0/1680664265579?e=2147483647&v=beta&t=Vtx9Z10ThBH9I78ZX3xIhTGrpqIK9sVUVelwd7HNbx8"
